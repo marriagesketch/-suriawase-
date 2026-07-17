07e1d1cb4a03cb06df047d7ba9c72d2b7ff7cb9a
tomorrowsketch <liffdevelop31257014@gmail.com>
2026-07-17 11:21:08 +0900

    Update app.js

--- app.js
+++ app.js
@@ -921,7 +921,7 @@ function renderViewMode(data, options = {}) {
      画像は1MB以下を推奨。PNGの透過部分はそのまま送ると
      反映されない場合があるため、白背景に合成したJPEGを使用する。
    ============================================================ */
-const HEADER_IMAGE_URL = "https://marriagesketch.github.io/-suriawase/image_message.jpg";
+const HEADER_IMAGE_URL = "https://marriagesketch.github.io/-suriawase-/image_message.jpg";
 
 function buildShareFlexMessage(shareName, shareURL) {
   const nameLine = shareName ? `${shareName}さんの回答が届きました` : "回答が届きました";
